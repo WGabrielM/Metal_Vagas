a819b7cbb420b86d5b7f17b6187e447eb6d4aa2c
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-07-17 11:25:08 -0300

    starting project and adding header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style/Header.css"
+
+export default function Header() {
+  return (
+  <div className="header">
+    <div>
+        Logo aqui
+    </div>
+    <div>
+        <ul>
+            <li>Link aqui</li>
+            <li>Link aqui</li>
+            <li>Link aqui</li>
+            <li>Link aqui</li>
+            <li><button>Anunciar Vagas</button></li>
+        </ul>
+    </div>
+  </div>
+  )
+}

5e601bac0c9ea59350722c030f405abd78fec9ca
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-07-19 12:01:30 -0300

    adding scroll in menu button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,45 @@
-import React from "react";
-import "./style/Header.css"
+import React, { useEffect, useRef } from "react";
+import "./style/Header.css";
+
+function abrirMenu() {
+  document.querySelector(".menu__button").addEventListener("click", () => {
+    document.querySelector(".menu__item").classList.add("ativo");
+  });
+}
+
 
 export default function Header() {
+  const activeSlideRef = useRef(null);
+
+  useEffect(() => {
+    if (activeSlideRef.current) {
+        activeSlideRef.current.scrollIntoView({
+          behavior: 'smooth',
+          block: 'nearest',
+          inline: 'nearest'
+        });
+    }
+  }, [activeSlide]);
+  
   return (
-  <div className="header">
-    <div>
-        Logo aqui
-    </div>
-    <div className="menu">
-        <button className="menu__button">
+    <div className="header">
+      <div>Logo aqui</div>
+      <div className="menu">
+        <button className="menu__button" ref={i === activeSlide ? activeSlideRef : null}>
           <span></span>
           <span></span>
           <span></span>
         </button>
-        <ul>
-            <li>Link aqui</li>
-            <li>Link aqui</li>
-            <li>Link aqui</li>
-            <li>Link aqui</li>
-            <li><button>Anunciar Vagas</button></li>
+        <ul className="menu__item">
+          <li className="menu__item--link">O que fazemos</li>
+          <li className="menu__item--link">Para empresas</li>
+          <li className="menu__item--link">Para profissionais</li>
+          <li className="menu__item--link">Como funciona</li>
+          <li className="menu__item--link">
+            <span>Anunciar Vagas</span>
+          </li>
         </ul>
+      </div>
     </div>
-  </div>
-  )
+  );
 }

672f381312bd7bd434b56219e316dfcc45efce26
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-07-21 15:01:51 -0300

    adding sytle in footer and images

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import "./style/Footer.css";
 
 export default function Footer() {
   return (
     <div className='footer'>
       <div>
         <p>Copyright © 2023 <span>MetalVagas</span></p>
+      </div>
+      <div>
+        <img src="/public/images/Footer/Facebook.png" alt="" />
+        <img src="/public/images/Footer/Twitter.png" alt="" />
+        <img src="/public/images/Footer/Instagram.png" alt="" />
+        <img src="/public/images/Footer/Linkedin.png" alt="" />
+        <img src="/public/images/Footer/Youtube.png" alt="" />
       </div>
         
       </div>

c94437842458a1a6226d41bc5f2573479791df3b
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-07-22 16:34:34 -0300

    starting config home]

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 import "./style/Footer.css";
 
 export default function Footer() {
   return (
-    <div className='footer'>
-      <div>
-        <p>Copyright © 2023 <span>MetalVagas</span></p>
+    <div className="footer">
+      <div className="footer__copy">
+        <p>
+          Copyright © 2023 <span>MetalVagas</span>
+        </p>
       </div>
-      <div>
-        <img src="/public/images/Footer/Facebook.png" alt="" />
-        <img src="/public/images/Footer/Twitter.png" alt="" />
-        <img src="/public/images/Footer/Instagram.png" alt="" />
-        <img src="/public/images/Footer/Linkedin.png" alt="" />
-        <img src="/public/images/Footer/Youtube.png" alt="" />
+      <div className="footer__social">
+        <img src="/public/images/Footer/Facebook.png" alt="Facebook" />
+        <img src="/public/images/Footer/Twitter.png" alt="Twitter" />
+        <img src="/public/images/Footer/Instagram.png" alt="Instagram" />
+        <img src="/public/images/Footer/Linkedin.png" alt="Linkedin" />
+        <img src="/public/images/Footer/Youtube.png" alt="Youtube" />
       </div>
-        
-      </div>
-  )
+    </div>
+  );
 }
-
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,24 +7,26 @@ export default function Header() {
   };
 
   return (
-    <div className="header">
-      <div className="logo"><p>Logo</p></div>
-      <div className="menu">
-        <button onClick={buttonClick} className="menu__button">
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
-        <ul className="menu__item">
-          <li className="menu__item--link">O que fazemos</li>
-          <li className="menu__item--link">Para empresas</li>
-          <li className="menu__item--link">Para profissionais</li>
-          <li className="menu__item--link">Como funciona</li>
-          <li className="menu__item--link">
-            <span>Anunciar Vagas</span>
-          </li>
-        </ul>
+    <>
+      <div className="header">
+        <div className="logo"><p>Logo</p></div>
+        <div className="menu">
+          <button onClick={buttonClick} className="menu__button">
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+          <ul className="menu__item">
+            <li className="menu__item--link">O que fazemos</li>
+            <li className="menu__item--link">Para empresas</li>
+            <li className="menu__item--link">Para profissionais</li>
+            <li className="menu__item--link">Como funciona</li>
+            <li className="menu__item--link">
+              <span>Anunciar Vagas</span>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 }

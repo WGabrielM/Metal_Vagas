fc940cdbaf1ca4a99c0091c1fae92841ad8d1dd1
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-08-04 10:21:24 -0300

    init whatwedo and download fonts

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Pages/Home";
 import NotFound from "./components/Pages/NotFound";
+import WhatWeDo from "./components/Pages/WhatWeDo";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/whatwedo" element={<WhatWeDo />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>

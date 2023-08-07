b2e637d81ba2f9cd6cc4a38216c60c3ae3d78ab5
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-08-07 15:02:11 -0300

    adding vídeo in whatwedo

--- src/components/Pages/WhatWeDo/index.jsx
+++ src/components/Pages/WhatWeDo/index.jsx
@@ -23,7 +23,12 @@ export default function WhatWeDo() {
 
           <button className="whatwedo__resume--link">Ver Mais</button>
         </div>
-        <div className="whatwedo__video"></div>
+        <div className="whatwedo__video">
+          <video width="100%" controls autoPlay muted loop>
+            <source src="/src/videos/oquefazemos.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
       </div>
     </>
   );

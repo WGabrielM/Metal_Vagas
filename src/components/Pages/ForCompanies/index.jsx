d957751aaddc4c7e69828c69fa42765004dc4c27
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-09-27 15:38:58 -0300

    add textArea in forCompanies

--- src/components/Pages/ForCompanies/index.jsx
+++ src/components/Pages/ForCompanies/index.jsx
@@ -1,28 +1,19 @@
 import React from "react";
 import "./style/ForCompanies.css";
+import TextArea from "../../TextArea";
 
 export default function ForCompanies() {
   return (
     <div className="forcompanies">
-      <div className="forcompanies__resume">
-        <h1 className="forcompanies__resume--title">
-          Para <span className="forcompanies__bold">empresas</span>
-        </h1>
-        <h2 className="forcompanies__resume--subtitle">
-          Encontre os melhores profissionais disponíveis no mercado.
-        </h2>
-        <p className="forcompanies__resume--text">
-          Unimos especialistas com mais de 20 anos de experiencia em diversas
-          modalidade:{" "}
-          <span className="forcompanies__bold">
-            Solda, Caldeiraria, Pintura, Mecânica etc
-          </span>{" "}
-          .. Adicionamos ao conhecimento deles Tecnologia e Inteligência
+      <TextArea
+          title="Para empresas"
+          subTitle="Encontre os melhores profissionais disponíveis no mercado."
+          text="Unimos especialistas com mais de 20 anos de experiencia em diversas
+          modalidade: Solda, Caldeiraria, Pintura, Mecânica etc.. Adicionamos ao conhecimento deles Tecnologia e Inteligência
           Artificial, criando uma ferramenta com filtros mais precisos, onde só
           o profissionais que atendam os requisitos da vaga anunciada, podem se
-          cadastrar. Evitando o desperdício de tempo e recursos.
-        </p>
-      </div>
+          cadastrar. Evitando o desperdício de tempo e recursos."
+        />
 
       <img
         className="forcompanies__image"

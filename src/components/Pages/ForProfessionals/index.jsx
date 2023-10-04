161932f9d6bd2f45c0754f719244348146301506
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-10-04 08:50:57 -0300

    add textArea in ForProfessional and fix fonts

--- src/components/Pages/ForProfessionals/index.jsx
+++ src/components/Pages/ForProfessionals/index.jsx
@@ -1,25 +1,22 @@
 import React from "react";
 import "./style/ForProfessionals.css";
+import TextArea from "../../TextArea";
 
 export default function ForProfessionals() {
   return (
     <div className="forprofessionals">
       <div className="forprofessionals__resume">
-        <h1 className="forprofessionals__resume--title">
-          Para <span className="forprofessionals__bold">profissionais</span>
-        </h1>
-        <h2 className="forprofessionals__resume--subtitle">
-          Fizemos várias pesquisas com os Profissionais da indústria
-          metalmecânica, e descobrimos:
-        </h2>
-        <p className="forprofessionals__resume--text">
-          Unimos especialistas com mais de 20 anos de experiencia em diversas
+        <TextArea
+          title="Para profissionais"
+          subTitle="Fizemos várias pesquisas com os Profissionais da indústria
+          metalmecânica, e descobrimos:"
+          text="Unimos especialistas com mais de 20 anos de experiencia em diversas
           modalidade: Solda, Caldeiraria, Pintura, Mecânica etc.. Adicionamos ao
           conhecimento deles Tecnologia e Inteligência Artificial, criando uma
           ferramenta com filtros mais precisos, onde só o profissionais que
           atendam os requisitos da vaga anunciada, podem se cadastrar. Evitando
-          o desperdício de tempo e recursos.
-        </p>
+          o desperdício de tempo e recursos."
+        />
       </div>
       
       <div className="forprofessionals__list">
